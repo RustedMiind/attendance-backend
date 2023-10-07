00fe82357f718b48681e89fd098692e751065e62
Ali Soliman <alisolimanworks@gmail.com>
2023-10-07 23:24:36 +0300

    removed unnecessary imports

--- src/controllers/user-controller/UserController.ts
+++ src/controllers/user-controller/UserController.ts
@@ -1,9 +1,3 @@
-import { Request, Response } from "express";
-import { successResponse, errorResponse } from "@/statics/responses";
-import prisma from "@/prisma";
-import validator from "validator";
-import bcrypt from "bcrypt";
-import { token } from "./createToken";
 import loginFunction from "./login";
 import createNewUserFunction from "./createNewUser";
 
