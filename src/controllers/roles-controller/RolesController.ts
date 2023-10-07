adf74d0cb2204623dcfdbebc10092cf73e0b4494
Ali Soliman <alisolimanworks@gmail.com>
2023-10-07 18:51:39 +0300

    update roles controller type name

--- src/controllers/roles-controller/RolesController.ts
+++ src/controllers/roles-controller/RolesController.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import { successResponse, errorResponse } from "@/statics/responses";
 import prisma from "@/prisma";
 
-type AccessType = number;
+type AccessIdType = number;
 
 export function createRole(req: Request, res: Response) {
   let name: string | null = req.body.name,
-    accesses: AccessType[] | null = req.body.accesses;
+    accesses: AccessIdType[] | null = req.body.accesses;
 
   if (name && accesses) {
     prisma.role
