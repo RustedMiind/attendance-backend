c67f2263f04c8941f71577f324a2e7e64dcd2d8a
Ali Soliman <alisolimanworks@gmail.com>
2023-10-10 07:57:38 +0300

    Check User Endpoint ready to be tested

--- src/controllers/user-controller/UserController.ts
+++ src/controllers/user-controller/UserController.ts
@@ -1,9 +1,11 @@
 import loginFunction from "./login";
 import createNewUserFunction from "./createNewUser";
 import assignRoleFunction from "./assignRole";
+import checkUser from "./checkUser";
 
 const createNewUser = createNewUserFunction;
 const login = loginFunction;
 const assignRole = assignRoleFunction;
+const checkUserByToken = checkUser;
 
-export default { createNewUser, login, assignRole };
+export default { createNewUser, login, assignRole, checkUserByToken };
---
+++ src/controllers/user-controller/checkUser.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+import { successResponse, errorResponse } from "@/statics/responses";
+import prisma from "@/prisma";
+import bcrypt from "bcrypt";
+import { token } from "./createToken";
+import jwt from "jsonwebtoken";
+
+function checkUser(req: Request, res: Response) {
+  const token = req.headers.authorization;
+
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY as string, function (err, tkn) {
+      if (err) {
+        res
+          .status(401)
+          .json(errorResponse(undefined, "Invalid token, Login Again"));
+      } else if (tkn) {
+        const tokenObj = tkn as { id: string };
+        prisma.userToken
+          .findFirst({
+            where: {
+              id: tokenObj.id,
+            },
+          })
+          .then((userToken) => {
+            console.log(userToken);
+            if (userToken) {
+              prisma.user
+                .findFirst({ where: { id: userToken.userId } })
+                .then((user) => {
+                  if (user) {
+                    const { password, ...noPassword } = user;
+                    res
+                      .status(202)
+                      .json(
+                        successResponse(
+                          { user: noPassword },
+                          "Logged in Successfully"
+                        )
+                      );
+                  } else {
+                    res
+                      .status(404)
+                      .json(
+                        errorResponse(
+                          undefined,
+                          "User Not found, It might be deleted, Try to login again"
+                        )
+                      );
+                  }
+                })
+                .catch((err) => {
+                  res
+                    .status(404)
+                    .json(
+                      errorResponse(
+                        err,
+                        "User Not found, It might be deleted, Try to login again"
+                      )
+                    );
+                });
+            }
+          })
+          .catch((err) => {
+            res.status(404).json(errorResponse(err, "Error parsing token"));
+          });
+      } else {
+        res.status(404).json(errorResponse(undefined, "Error parsing token"));
+      }
+    });
+  } else {
+    res
+      .status(401)
+      .json(errorResponse(undefined, "No Login Sessions, Login then proceed"));
+  }
+}
+
+export default checkUser;
