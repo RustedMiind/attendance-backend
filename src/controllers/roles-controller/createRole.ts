853e24fffdf22821c693f8f47791c391dc5d6991
Ali Soliman <alisolimanworks@gmail.com>
2023-10-13 23:23:56 +0300

    Create role test Now is the currenct create role

--- src/controllers/roles-controller/createRole.ts
+++ src/controllers/roles-controller/createRole.ts
@@ -2,36 +2,87 @@ import { Request, Response } from "express";
 import { successResponse, errorResponse } from "@/statics/responses";
 import prisma from "@/prisma";
 import { idsArrayToPermissionObj } from "./idsArrayToPermissionObj";
+import { checkIsUserWithCallback } from "@/middlewares/checkUser";
+import { removeDuplicates } from "@/functions/removeDuplicates";
+import { Prisma } from "@prisma/client";
+import { getNoPermissionTo } from "./functions/getNoPermissionTo";
+import CreateRolePromise from "./functions/createRolePromise";
 
 type PermissionIdType = number;
 
 function createRoleFunction(req: Request, res: Response) {
-  let name: string | null = req.body.name,
-    permissions: PermissionIdType[] | null = req.body.permissions;
+  const name: string | null = req.body.name,
+    permissions: PermissionIdType[] | null = removeDuplicates(
+      req.body.permissions
+    );
 
-  if (name && permissions) {
-    prisma.role
-      .create({
-        data: {
-          name,
-          permissions: {
-            connect: idsArrayToPermissionObj(permissions),
-          },
-        },
-        include: {
-          permissions: {
-            include: { action: { select: { value: true, name: true } } },
-          },
-        },
-      })
-      .then((result) => {
-        res.status(200).json(successResponse(result));
-      })
-      .catch((err) => {
-        res.status(400).json(errorResponse(err));
-      });
+  if (
+    name &&
+    typeof name === "string" &&
+    permissions &&
+    Array.isArray(permissions)
+  ) {
+    checkIsUserWithCallback(req, res, (user) => {
+      if (user) {
+        prisma.permission
+          .findMany({
+            where: { OR: idsArrayToPermissionObj(permissions) },
+            include: { action: true },
+          })
+          .then((resultPermissions) => {
+            if (permissions.length === resultPermissions.length && user.role) {
+              // If Requested permissions all found
+              // res.json(resultPermissions);
+              const noPermissionTo = getNoPermissionTo(
+                user.role.permissions,
+                resultPermissions
+              );
+              if (noPermissionTo.length === 0) {
+                //  Here he can Create the role needed
+                CreateRolePromise({ name, resultPermissions })
+                  .then((result) => {
+                    res.status(200).json(successResponse(result));
+                  })
+                  .catch((err) => {
+                    res.status(400).json(errorResponse(err));
+                  });
+              } else {
+                res.status(402).json(
+                  errorResponse(
+                    {
+                      permissionDenied: noPermissionTo,
+                    },
+                    "Some Permissions the user doesn't have access to give"
+                  )
+                );
+              }
+            } else {
+              res
+                .status(404)
+                .json(
+                  errorResponse(
+                    undefined,
+                    `Permissions requested are ${permissions.join(
+                      ", "
+                    )}, but the permissions found are ${resultPermissions
+                      .map((p) => p.id)
+                      .join(", ")}`
+                  )
+                );
+            }
+          })
+          .catch((err) => {
+            res
+              .status(400)
+              .json(
+                errorResponse(err, "Error Finding the requested permissions")
+              );
+          });
+      }
+    });
   } else {
-    res.status(400).json(errorResponse(undefined, "Please Enter Valid Inputs"));
+    res.status(401).json(errorResponse(undefined, "Please Enter Valid Inputs"));
   }
 }
+
 export { createRoleFunction };
